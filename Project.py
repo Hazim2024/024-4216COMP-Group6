da5f9c1d621a4846daf208c96ceffde5273cbcb0
Hazim2024 <hazimiftikhar@gmail.com>
2024-04-15 01:09:37 +0100

    Added comments

--- Project.py
+++ Project.py
@@ -2,15 +2,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_csv("cbb.csv")
-
-average_offensive_efficiency = data.groupby(['YEAR', 'TEAM'])['ADJOE'].mean().unstack()
-
-top_5_teams = average_offensive_efficiency.mean().nlargest(5).index
-
-average_offensive_efficiency_top_5 = average_offensive_efficiency[top_5_teams]
-
-plt.figure(figsize=(12, 8))
-average_offensive_efficiency_top_5.plot(kind='bar', width=0.8)
+#Calculate average offensive efficiency for every team for each year
+averageOffensive = data.groupby(['YEAR', 'TEAM'])['ADJOE'].mean().unstack()
+# Get the top 5 teams with the highest average
+top_5 = averageOffensive.mean().nlargest(5).index
+#Filter the data to include only the top 5 teams
+averageOffensiveTop_5 = averageOffensive[top_5]
+#Plot the bar graph
+averageOffensiveTop_5.plot(kind='bar', width=0.8)
 plt.title('Average Offensive Efficiency of Top 5 Teams Over the Years')
 plt.xlabel('Year')
 plt.ylabel('Average Offensive Efficiency (ADJOE)')
