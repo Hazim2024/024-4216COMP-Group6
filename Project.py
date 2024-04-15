d375dccde09de7a48cce6c715bb4a5e3d3b692a0
Hazim2024 <hazimiftikhar@gmail.com>
2024-04-15 23:31:48 +0100

    Added new graph

--- Project.py
+++ Project.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_csv("cbb.csv")
+
 #Calculate average offensive efficiency for every team for each year
 averageOffensive = data.groupby(['YEAR', 'TEAM'])['ADJOE'].mean().unstack()
 # Get the top 5 teams with the highest average
@@ -16,4 +17,22 @@ plt.ylabel('Average Offensive Efficiency (ADJOE)')
 plt.legend(title='Team', bbox_to_anchor=(1, 1))
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.tight_layout()
+plt.show()
+
+
+# Average Three Pointer Comparision between 2nd and Champion Teams
+champion_teams = data[data['POSTSEASON'] == 'Champions']
+second_place_teams = data[data['POSTSEASON'] == '2ND']
+# calculate the average three-pointer shots for champion teams
+chamAvg3P = champion_teams.groupby('YEAR')['3P_O'].mean().reset_index()
+# Calculate the average three-pointer shots for second-place teams
+secondPlace3P = second_place_teams.groupby('YEAR')['3P_O'].mean().reset_index()
+
+fig, ax = plt.subplots(figsize=(10, 8))
+ax.barh(chamAvg3P['YEAR'], chamAvg3P['3P_O'], color='blue', label='Champion Teams')
+ax.barh(secondPlace3P['YEAR'], secondPlace3P['3P_O'], color='orange', label='Second Place Teams')
+ax.set_xlabel('Average Three-Pointer Shots')
+ax.set_ylabel('Year')
+ax.set_title('Average Three-Pointer Shots Comparison')
+ax.legend()
 plt.show()
