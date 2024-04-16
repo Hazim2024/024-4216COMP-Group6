56d073c66e1a0f35c742d3119c89831c662dcf9a
Hazim2024 <hazimiftikhar@gmail.com>
2024-04-16 01:50:45 +0100

    Update

--- Project.py
+++ Project.py
@@ -18,21 +18,3 @@ plt.legend(title='Team', bbox_to_anchor=(1, 1))
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.tight_layout()
 plt.show()
-
-
-# Average Three Pointer Comparision between 2nd and Champion Teams
-champion_teams = data[data['POSTSEASON'] == 'Champions']
-second_place_teams = data[data['POSTSEASON'] == '2ND']
-# calculate the average three-pointer shots for champion teams
-chamAvg3P = champion_teams.groupby('YEAR')['3P_O'].mean().reset_index()
-# Calculate the average three-pointer shots for second-place teams
-secondPlace3P = second_place_teams.groupby('YEAR')['3P_O'].mean().reset_index()
-
-fig, ax = plt.subplots(figsize=(10, 8))
-ax.barh(chamAvg3P['YEAR'], chamAvg3P['3P_O'], color='blue', label='Champion Teams')
-ax.barh(secondPlace3P['YEAR'], secondPlace3P['3P_O'], color='orange', label='Second Place Teams')
-ax.set_xlabel('Average Three-Pointer Shots')
-ax.set_ylabel('Year')
-ax.set_title('Average Three-Pointer Shots Comparison')
-ax.legend()
-plt.show()
