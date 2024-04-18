2b7864fd878bbc935467d80cc21d7309773f7c84
jordyAudon <156800062+jordyAudon@users.noreply.github.com>
2024-04-18 23:25:33 +0100

    Create jordy-visualisation.py

---
+++ jordy-visualisation.py
@@ -0,0 +1,8 @@
+ data = pd.read_csv("cbb.csv")
+    average_winning_ratio = data.groupby('TEAM')['W'].mean()
+    top_5_teams = average_winning_ratio.nlargest(5)
+    plt.figure(figsize=(10, 6))
+    plt.pie(top_5_teams, labels=top_5_teams.index, autopct='%1.1f%%', startangle=140)
+    plt.title('Top 5 Teams with Highest Winning Ratio')
+    plt.axis('equal')
+    plt.show()
